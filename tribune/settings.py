53a2e120188b223888b3e49962fa3ff3de49b1c8
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-02 21:44:00 +0300

    Add changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staicfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
